596ef4e05bd451b3046409140abb67d80232597c
Felipe Nascimento <flpnascto@gmail.com>
2021-06-27 00:44:14 -0300

    swagger for routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,15 @@ const routes = require('express').Router();
 const developerRoute = require('./developerRoute');
 const specialtyRoute = require('./specialtyRoute');
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Developers
+ *     description: The developer managing API
+ *   - name: Specialties
+ *     description: Access all specialties indexed in API
+ */
+
 routes.use('/developers', developerRoute);
 routes.use('/specialties', specialtyRoute);
 
