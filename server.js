e3c2a300dfd5637135012e142ba73e03b52a92de
Felipe Nascimento <flpnascto@gmail.com>
2021-06-27 00:43:32 -0300

    add swagger documentation

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const specs = require('./config/swaggerSpecs');
 const routes = require('./routes');
 const errorController = require('./controllers/errorController');
+require('dotenv').config();
+
+const { PORT } = process.env;
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
+app.listen(PORT, () => console.log('ouvindo porta 3000!'));
 
 app.get('/', (_request, response) => {
   response.send('Olá');
